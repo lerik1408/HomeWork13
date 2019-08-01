ae307dac78d5abb46341c9e6e3a1b4ccbf25adfe
ilya <popruzhuk.38@gmail.com>
2019-08-01 15:02:09 +0300

    fix

--- src/components/siginUp/createAcc/form.vue
+++ src/components/siginUp/createAcc/form.vue
@@ -1,10 +1,10 @@
 <template>
     <main class="main">
-        <div class="main__wrap">
+        <div @click="show($event)" class="main__wrap">
             <form action="" method="get" class="form">
                 <h3 class="form__headline">Complete your account</h3>
-                <input type="text" class="form__input" placeholder="Create a password" id="pass">
-                <div class="bar">
+                <input @input="check($event)" type="text" class="form__input" placeholder="Create a password" id="pass">
+                <div v-if="showFlag" class="bar">
                     <div class="bar__item"></div>
                     <div class="bar__item"></div>
                     <div class="bar__item"></div>
@@ -21,7 +21,6 @@
                     </div>
                 </div>
                 <button class="form__button">Get started</button>
-                <p>{{mess}}</p>
             </form>
         </div>
     </main>
@@ -30,13 +29,54 @@
 export default {
     data: function () {
         return {
-            mess: 0
+            showFlag: false
         }
     },
     methods: {
-        kk: function(){
-            console.log('dwdw')
-        }
+        show: function(event){
+            const input = document.getElementById('pass');
+            if(event.target===input){
+                this.showFlag = true
+            }else{
+                this.showFlag = false
+            }
+        },
+        check: function(event){
+            const bar = document.getElementsByClassName('bar__item');
+            const bars = document.getElementsByClassName('bar');
+            const valueArr = event.target.value.split('');
+            if (valueArr.length === 0) {
+            bars[0].classList.remove('bar--active');
+            } else if (valueArr.length === 1) {
+            for (let i = 0; i < bar.length; i += 1) {
+                bars[0].classList.add('bar--active');
+                bar[i].classList.remove('bar__item--low');
+                bar[i].classList.remove('bar__item--normal');
+                bar[i].classList.remove('bar__item--good');
+            }
+            } else if (valueArr.length <= 3) {
+            for (let i = 0; i < bar.length; i += 1) {
+                bar[i].classList.remove('bar__item--normal');
+                bar[i].classList.remove('bar__item--good');
+            }
+            bar[0].classList.add('bar__item--low');
+            } else if (valueArr.length <= 5) {
+            for (let i = 0; i < bar.length; i += 1) {
+                bar[i].classList.remove('bar__item--low');
+                bar[i].classList.remove('bar__item--good');
+            }
+            bar[0].classList.add('bar__item--normal');
+            bar[1].classList.add('bar__item--normal');
+            } else {
+            for (let i = 0; i < bar.length; i += 1) {
+                bar[i].classList.remove('bar__item--low');
+                bar[i].classList.remove('bar__item--normal');
+            }
+            bar[0].classList.add('bar__item--good');
+            bar[1].classList.add('bar__item--good');
+            bar[2].classList.add('bar__item--good');
+            }
+                }
     }
 }
 </script>
@@ -161,7 +201,8 @@ export default {
 .bar
     width: 100%
     height: 6px
-    display: none
+    // display: none
+    display: flex
     background-color: #fff
     margin-bottom: 21px
 .bar--active
